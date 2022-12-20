eaed87de363cc6c289d8ceebd316a7553eaa60f5
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-12-20 21:52:15 +0100

    Create has-many and belongs-to relationship

--- student_object.rb
+++ student_object.rb
@@ -1,9 +1,17 @@
 require_relative 'person_object'
+require_relative 'classroom_object'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom = 'Classroom 1')
     super(age)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
