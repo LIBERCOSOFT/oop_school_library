bd7b8881389f3d097c0b354cc4090f810ae806b2
ouasamine <ouas.amine@outlook.com>
2023-01-02 14:58:55 +0100

    Place app starter method in main.rb

--- app.rb
+++ app.rb
@@ -10,39 +10,8 @@ class App
     @rentals = []
   end
 
-  puts 'Welcome to School Library App!'
-
-  def start_app
-    puts "\n"
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-
-    menu_option = user_input
-    option_selection(menu_option)
-  end
-
   def user_input
     gets.chomp
-  end
-
-  def option_selection(option)
-    case option
-    when '1' then list_books
-    when '2' then list_people
-    when '3' then create_person
-    when '4' then create_book
-    when '5' then create_rentals
-    when '6' then list_rentals_per_id
-    when '7' then puts 'Thank you for using this app!'
-    else puts 'Invalid number selected, Please pick from 1 - 7'
-    end
-    start_app unless option == '7'
   end
 
   def create_person
